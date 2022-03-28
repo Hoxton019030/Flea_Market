9d015b97955476e13fae40624e991744084d0031
Hoxton <za546321@yahoo.com.tw>
2022-03-29 01:40:27 +0800

    2022-3-29

--- WebApp_day_4/src/main/java/CookieGet.java
+++ WebApp_day_4/src/main/java/CookieGet.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.catalina.util.URLEncoder;
-
 @WebServlet("/CookieGet")
 public class CookieGet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
--- WebApp_day_4/src/main/java/CookieSet.java
+++ WebApp_day_4/src/main/java/CookieSet.java
@@ -28,3 +28,4 @@ public class CookieSet extends HttpServlet {
 	}
 
 }
+
